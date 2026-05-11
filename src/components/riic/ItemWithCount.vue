9ce522788f80f2dc0fec7544d9fce0903518bd0a
BioHazard <3482991796@qq.com>
2026-05-12 02:20:00 +0800

    feat: 优化物品数量显示

--- src/components/riic/ItemWithCount.vue
+++ src/components/riic/ItemWithCount.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
 import { getItemIconUrl, getItemName } from '@/utils/item';
+import { computed } from 'vue';
 
 const props = defineProps<{
   itemId: string;
   count: number | null;
 }>();
+
+const formatter = new Intl.NumberFormat(undefined, {
+  notation: 'compact',
+  compactDisplay: 'short',
+});
+
+const itemName = computed(() => getItemName(props.itemId));
+const tooltipText = computed(() => {
+  if (props.count !== null) {
+    return `${itemName.value}×${props.count}`;
+  } else {
+    return itemName.value;
+  }
+});
 </script>
 
 <template>
-  <UTooltip :text="getItemName(props.itemId)">
+  <UTooltip :text="tooltipText">
     <div class="relative inline-block">
       <img
-        :alt="getItemName(props.itemId)"
+        :alt="itemName"
         class="h-full w-full object-contain"
         referrerpolicy="no-referrer"
         :src="getItemIconUrl(props.itemId)"
@@ -20,7 +35,7 @@ const props = defineProps<{
         v-if="props.count !== null"
         class="text-md absolute inset-e-0 inset-be-0 rounded bg-default/75 px-0.5 leading-tight font-semibold text-default backdrop-blur-xs"
       >
-        {{ props.count }}
+        {{ formatter.format(props.count) }}
       </div>
     </div>
   </UTooltip>
