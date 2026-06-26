e1a33705a8d5f73e1dc2bf994dcc2faf8274598b
BioHazard <3482991796@qq.com>
2026-06-26 09:59:35 +0800

    perf: 把字体拆分出单独的 CSS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import '@/assets/css/main.scss';
+import App from '@/App.vue';
 import '@/assets/css/base.css';
+import '@/assets/css/main.scss';
+import router from '@/router';
 import ui from '@nuxt/ui/vue-plugin';
 import { createApp } from 'vue';
-import App from '@/App.vue';
-import router from '@/router';
+
+// 动态导入字体 CSS，构建时生成单独的 CSS 文件
+import('@/assets/css/fonts.css');
 
 const app = createApp(App);
 
