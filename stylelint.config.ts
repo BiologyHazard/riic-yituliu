777cb1903de9cd4d2dd0e8bb19b24ad57e220a37
BioHazard <3482991796@qq.com>
2026-06-28 04:32:53 +0800

    chore: 移除 sass 相关依赖，更新 stylelint 配置

--- stylelint.config.ts
+++ stylelint.config.ts
@@ -1,29 +1,20 @@
 import type { Config } from 'stylelint';
 
 export default {
-  extends: [
-    'stylelint-config-standard',
-    'stylelint-config-standard-scss',
-    'stylelint-config-standard-vue/scss',
-    'stylelint-config-html',
-    'stylelint-config-recess-order',
-  ],
+  extends: ['stylelint-config-standard', 'stylelint-config-html', 'stylelint-config-recess-order'],
   ignoreFiles: ['**/dist/**', '**/node_modules/**', 'src/assets/fonts/**/*.css'],
   overrides: [
     {
       files: ['**/*.vue'],
       customSyntax: 'postcss-html',
     },
-    {
-      files: ['**/*.scss'],
-      customSyntax: 'postcss-scss',
-    },
   ],
   rules: {
-    'custom-property-empty-line-before': null,
+    'at-rule-no-unknown': null,
     'color-hex-length': 'long',
+    'comment-empty-line-before': null,
+    'custom-property-empty-line-before': null,
     'font-family-name-quotes': 'always-unless-keyword',
-    'scss/at-rule-no-unknown': null,
-    'scss/double-slash-comment-empty-line-before': null,
+    'import-notation': 'string',
   },
 } satisfies Config;
